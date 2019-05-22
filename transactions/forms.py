b76a574c75991260dc7de775c9a3fdf70cb18569
hlorofilka <hlorofilk@mail.ru>
2019-05-22 17:44:50 +0200

    CRUD accounts, upgrade Transaction

--- transactions/forms.py
+++ transactions/forms.py
@@ -4,10 +4,20 @@ from django import forms
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-from .models import Transaction
+from django.utils import timezone
+from .models import Transaction, Account
 
 
 class EditTransactionForm(ModelForm):
     class Meta:
         model = Transaction
-        fields = ('created_date', 'transaction_type', 'amount', 'comment')
+        fields = ('created_date', 'transaction_type', 'account', 'amount', 'comment')
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(EditTransactionForm, self).__init__(*args, **kwargs)
+        self.fields["account"].queryset = Account.objects.filter(holder=self.request.user)
+
+class CreateAccountForm(forms.Form):
+    created_date = forms.DateTimeField(initial=timezone.now)
+    title = forms.CharField(max_length=200)
+    initial_balance = forms.FloatField()
--- transactions/models.py
+++ transactions/models.py
@@ -4,12 +4,21 @@ from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Account(models.Model):
+    created_date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200)
+    holder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, default = None)
+    def __str__(self):
+        return self.title
+
 class Transaction(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, default = None)
     transaction_type_choice = (('-', 'expense'), ('+', 'income'))
     transaction_type = models.CharField(max_length=1, choices=transaction_type_choice)
-    amount = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(999999999999.99)],)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True, default = None)
+    amount = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(999999999999.99)], null=True, blank=True, default = 0)
     comment = models.CharField(max_length=200)
+    is_initial = models.BooleanField(default=False)
     def __str__(self):
         return self.transaction_type+str(self.amount)+ " "+self.comment
